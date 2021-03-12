54ea2dd375193ae923114c70d3f926a7c6864ce6
Kingsley Bawuah <Kbawuah@caring.com>
2021-03-11 19:55:12 -0500

    Added cobra, moved existing code to get cmd.

--- go.mod
+++ go.mod
@@ -2,4 +2,9 @@ module github.com/KingsleyBawuah/go-torrent
 
 go 1.15
 
-require github.com/zeebo/bencode v1.0.0
+require (
+	github.com/mitchellh/go-homedir v1.1.0
+	github.com/spf13/cobra v1.1.3
+	github.com/spf13/viper v1.7.0
+	github.com/zeebo/bencode v1.0.0
+)
--- main.go
+++ main.go
@@ -1,57 +1,22 @@
+/*
+Copyright © 2021 Kingsley Bawuah <Kingsley404@gmail.com>
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
 package main
 
-import (
-	"bytes"
-	"github.com/zeebo/bencode"
-	"io/ioutil"
-	"log"
-	"os"
-)
+import "github.com/KingsleyBawuah/go-torrent/cmd"
 
 func main() {
-	log.Println("Welcome to go-torrent!")
-	torrentFilepath := os.Args[1]
-
-	//Find and Open the file.
-	torrentFile, err := os.Open(torrentFilepath)
-	if err != nil {
-		if err == err.(*os.PathError) {
-			log.Panic("Error finding your file. Are you sure this is the correct path?", torrentFilepath)
-		}
-		log.Panic("Error opening file: ", err)
-	}
-	defer torrentFile.Close()
-
-	//Read the file
-	torrentFileContent, err := ioutil.ReadFile(torrentFilepath)
-	if err != nil {
-		log.Panic("Error reading file: \n", err)
-	}
-
-	//Information on torrent file structure: https://wiki.theory.org/BitTorrentSpecification
-	type torrentInfoBencode struct {
-		Pieces      string `bencode:"pieces"`       //string consisting of the concatenation of all 20-byte SHA1 hash values, one per piece.
-		PieceLength int64  `bencode:"piece length"` //Bytes per piece
-	}
-
-	type torrent struct {
-		Announce     string             `bencode:"announce"`      //Url of the tracker
-		Comment      string             `bencode:"comment"`       //Comment left on the torrent. Optional
-		CreationDate int64              `bencode:"creation date"` //Date torrent was created
-		CreatedBy    string             `bencode:"created by"`    //Author. Optional
-		Encoding     string             `bencode:"encoding"`      //the string encoding format used to generate the pieces part of the info dictionary in the .torrent metafile. Optional
-		Info         torrentInfoBencode `bencode:"info"`          //TODO: Handle multiple file case.
-	}
-
-	inputTorrentFile := &torrent{}
-
-	//Decode the file.
-	r := bytes.NewReader(torrentFileContent)
-	decoder := bencode.NewDecoder(r)
-
-	if err := decoder.Decode(inputTorrentFile); err != nil {
-		log.Panic("Error decoding torrent file: ", err)
-	}
-
-	log.Printf("Decoded torrent file %+v\\n", inputTorrentFile)
+	cmd.Execute()
 }
